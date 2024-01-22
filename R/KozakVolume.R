9ab5573c1f802c5ce3871648be764c6ec4317c63
Mike Premer <michael.premer@maine.edu>
2024-01-22 11:00:15 -0500

    updates

---
+++ R/KozakVolume.R
@@ -0,0 +1,89 @@
+#' Kozak Volume Equation
+#'
+#' This function allows you to calculate the volume at any point on tree
+#' for common tree species found in the northeastern and acadian forests.
+#' All measurements must be entered as metric. Calculates volume by breaking
+#' tree into 100 sections using the kozak taper function and calculating volume
+#' of each section using the Smalians volume formula.
+#'
+#' @param Bark specify if you want measurement inside or outside bark: 'ob' = outside, 'ib' = inside.
+#' @param SPP Species: use FVS species codes: example 'RO' - Red Oak, 'WS' = White Spruce
+#' @param DBH Diameter Breast Height in cm
+#' @param HT Tree Height in meters
+#' @param Planted specify if the tree is planted, specify as TRUE or FALSE, defaults to FALSE
+#' @param stump the height of the stumpin meters you want excluded from the volume measurement, defaults to NA.
+#' @param topHT Top height of crown in meters, defaults to NA
+#' @param topD The diameter in cm at which you want the volume measurement to stop, defaults to NA.
+#'
+#' @seealso [MEForLab::KozakTaper]
+#' @family Volume Functions
+#' @references
+#'Kozak, A. (2004). My last words on taper equations. The Forestry Chronicle,
+#'80(4), 507–515. https://doi.org/10.5558/tfc80507-4
+#'
+#'Li, R., Weiskittel, A., Dick, A. R., Kershaw, J. A., & Seymour, R. S. (2012).
+#'Regional Stem Taper Equations for Eleven Conifer Species in the Acadian Region of
+#'North America: Development and Assessment. Northern Journal of Applied Forestry,
+#'29(1), 5–14. https://doi.org/10.5849/njaf.10-037
+#'
+#'MacFarlane, D. W., & Weiskittel, A. R. (2016). A new method for capturing stem taper
+#'variation for trees of diverse morphological types. Canadian Journal of Forest Research,
+#'46(6), 804–815. https://doi.org/10.1139/cjfr-2016-0018
+#'
+#'Weiskittel, A., & Li, R. (n.d.). DEVELOPMENT OF REGIONAL TAPER AND VOLUME EQUATIONS: HARDWOOD SPECIES. 8.
+#' @details
+#' Originally coded by Ryan Smith, UMO, SILC
+#'
+#' @examples
+#'
+#' KozakTreeVol(Bark = 'ib', SPP = 'RO', DBH = 40, HT = 18, Planted = FALSE, stump = .5, topHT = NA, topD = 25.3)
+#' KozakTreeVol('ob', 'WP', 32.5, 21)
+#'
+#' @export
+
+KozakTreeVol <- function(Bark, SPP, DBH, HT, Planted = FALSE,stump=NA,topHT=NA,topD=NA){
+  sgmts <- 100
+  stump <- ifelse(is.na(stump),as.numeric(0.0),stump)
+  topHT <- ifelse(is.na(topHT),as.numeric(HT),topHT)
+  topHT <- ifelse(topHT>HT,as.numeric(HT),topHT)
+  topD <- (ifelse(is.na(topD),as.numeric(0.001),topD))
+  L <- (topHT - stump)/sgmts
+  i <- 0
+  treeVolume <- 0
+  while (i < sgmts)
+  {
+    H1 <- L * i + stump
+    H2 <- L * (i + 1) + stump
+    if (HT - H1 < 1e-04){
+      dob1 <- 0
+      dib1 <- 0
+    }
+    else {
+      if (H1 == 0)
+        H1 <- 0.001
+      Esty1 <- KozakTaper(Bark='ob',SPP=SPP,DHT=H1,DBH=DBH,HT=HT,Planted=Planted)
+      dob1 <- as.numeric(Esty1)
+      dob1 <- ifelse(dob1<topD,0,dob1)
+      dib1 <- DOBtoDIB(SPP=SPP,dob=dob1)
+      dib1 <- ifelse(dob1<topD,0,dib1)
+    }
+    if (HT - H2 < 1e-04){
+      dob2 <- 0
+      dib2 <- 0
+    }
+    else {
+      if (H2 == 0)
+        H2 <- 0.001
+      Esty2 <- KozakTaper(Bark='ob',SPP=SPP,DHT=H2,DBH=DBH,HT=HT,Planted=Planted)
+      dob2 <- as.numeric(Esty2)
+      dob2 <- ifelse(dob2<topD,0,dob2)
+      dib2 <- DOBtoDIB(SPP=SPP,dob=dob2)
+      dib2 <- ifelse(dob1<topD,0,dib2)
+    }
+    treeVolume <- ifelse(Bark=='ob',treeVolume + Smalians(dob1, dob2, L * 100),
+                         treeVolume + Smalians(dib1, dib2, L * 100))
+    i <- i + 1
+  }
+  treeVolume <- round(treeVolume/1e+06, 6)
+  return(treeVolume <- treeVolume)
+}
---
+++ R/MaxCrownWidth.R
@@ -0,0 +1,76 @@
+#' Maximum Crown Width
+#'
+#' This function returns the maximum crown width for an open grown tree
+#' for the most common species found in the acadian forest.
+#' ##
+#' Metric
+#' ##
+#'
+#'###
+#' SPP included: BF, BS, EH, WP, NC, RS, WS, AB, GB, RB, RO, PB, QA, RM, SM, YB.
+#' SPP that are not included can be enter as 'OH' for hardwoods and 'OS' for softwoods.
+#' If no SPP is entered a standard coefficient will be used but may not reflect the
+#' crown conditions accurately - please enter 'OS' or 'OH' if species is not included.
+#'### MCW is a dependency needed to calculate LCW.
+#'
+#'@seealso [inventoryfunctions::LCW]
+#'
+#'@family Crown Functions
+#'@param SPP Tree Species: use the FVS code
+#'@param DBH Diameter at breast height in cm.
+#'
+#'@return This function returns an numeric vector length n providing the maximum crown width value for n trees in meters.
+#'
+#'@references
+#'Russell, M. B., & Weiskittel, A. R. (2011). Maximum and Largest Crown Width Equations for 15 Tree Species in Maine.
+#'Northern Journal of Applied Forestry, 28(2), 84–91. https://doi.org/10.1093/njaf/28.2.84
+#'@details
+#'Originally coded by Ryan Smith, UMO, SILC
+
+#'@examples
+#'
+#'MCW("BF", 25)
+#'
+#'SPP   <- c("BF", "RO", "RS", "YB", "RO", "YB")
+#'DBH   <- c(24, 34, 18, 41, 6, 20)
+#'
+#'example <- mapply(MCW, SPP, DBH)
+#'
+#' \dontrun{
+#' Tibble %>% mutate(MCW = MCW("SPP Variable", "DBH Variable"))
+#' }
+#'
+#'@export
+
+
+MCW <- function(SPP,DBH)
+{
+  SPcodes=c('BF','BS','EH','WP','NC','RS','WS','AB','GB','RB','RO','PB','QA',
+            'RM','SM','YB','OH','OS','99')
+  coefs = matrix(c(
+    # a1           a2
+    1.37       , 0.572      ,    # BF
+    0.535      , 0.742      ,    # BS
+    2.44       , 0.408      ,    # EH
+    1.24       , 0.585      ,    # WP
+    1.63       , 0.436      ,    # NC
+    1.80       , 0.461      ,    # RS
+    1.50       , 0.496      ,    # WS
+    2.93       , 0.434      ,    # AB
+    2.24       , 0.382      ,    # GB
+    2.24       , 0.382      ,    # RB
+    4.08       , 0.310      ,    # RO
+    1.48       , 0.623      ,    # PB
+    1.31       , 0.586      ,    # QA
+    2.17       , 0.491      ,    # RM
+    3.31       , 0.356      ,    # SM
+    4.04       , 0.308      ,    # YB
+    4.04       , 0.308      ,    # OH
+    1.597128571, 0.513957143,    # OS
+    2.24262    , 0.462653333),   # 99
+    ncol=2,byrow=TRUE)
+  sprow = match(SPP,SPcodes)
+  sprow[is.na(sprow)] = length(SPcodes)
+  X <- coefs[sprow,1]*DBH**coefs[sprow,2]
+  return(X)
+}
