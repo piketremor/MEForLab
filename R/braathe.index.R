a1aa7bfd86c57e447d710cea7f6d943dced0ef2a
PikeTremor <115101199+piketremor@users.noreply.github.com>
2024-03-04 22:34:00 -0500

    bummer

--- R/braathe.index.R
+++ R/braathe.index.R
@@ -10,7 +10,6 @@
 #' @author Premer, M.I. - Maine Forest Lab
 #' @references Braathe, P., 1980. Height increment of young single trees in relation to height and distance of neighboring trees. Mitt. Forstl. VersAnst. 130, 43–48.
 
-
 braathe.index <- function(comp.ht,focal.ht,distance){
   braathe.index = comp.ht/(focal.ht*distance)
   return(braathe.index)
